ed8513359fdc56e65dc5916c52002811ba2f16b2
近藤 晴輝 <65643010+HarukiKondo@users.noreply.github.com>
2025-11-12 16:45:39 +0900

    Update Country.java

--- src/main/java/eu/stenlund/Country.java
+++ src/main/java/eu/stenlund/Country.java
@@ -1,13 +1,21 @@
 package eu.stenlund;
 
+/**
+ * シンプルなCountryクラス
+ */
 public class Country {
 
+    // 名前
     public String name;
+    // 説明
     public String description;
-
+    
     public Country() {
     }
 
+    /**
+     * コンストラクター
+     */
     public Country(String name, String description) {
         this.name = name;
         this.description = description;
